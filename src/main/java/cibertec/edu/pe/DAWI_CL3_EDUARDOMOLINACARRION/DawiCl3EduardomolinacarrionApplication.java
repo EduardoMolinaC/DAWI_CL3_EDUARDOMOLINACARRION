2d7f72d26d786f47cf3d06baeed57d855ca5bbc6
Tryprex <tryprex@gmail.com>
2023-11-26 14:26:56 -0500

    primer commit

---
+++ src/main/java/cibertec/edu/pe/DAWI_CL3_EDUARDOMOLINACARRION/DawiCl3EduardomolinacarrionApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWI_CL3_EDUARDOMOLINACARRION;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3EduardomolinacarrionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3EduardomolinacarrionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DAWI_CL3_EDUARDOMOLINACARRION/DawiCl3EduardomolinacarrionApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DAWI_CL3_EDUARDOMOLINACARRION;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3EduardomolinacarrionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
